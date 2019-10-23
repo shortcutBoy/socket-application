2b557d3b67a81e1b445240ba79613e8a23636d6b
huang.zw <turbohzw@icloud.com>
2019-10-23 18:53:25 +0800

    feat: add new im-page editor.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import { App } from './App';
 import * as serviceWorker from './serviceWorker';
 
-export const socket = io.connect('http://192.168.0.138:4000');
+export const socket = io.connect('http://192.168.98.68:4000');
 ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/router/editor/index.js
+++ src/router/editor/index.js
@@ -1,16 +1,44 @@
 import React from 'react';
+import { socket } from '../../index';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import './index.css';
 
 export class Editor extends React.Component {
-  componentDidMount () {}
+  constructor(props) {
+    super(props);
+    this.state = {data: '<p>Hello!</p>'};
+  }
+  componentDidMount() {
+    socket.on('editing', (msg) => {
+      this.setState({
+        data: msg.data,
+      });
+    });
+  }
+  onChange = ( event, editor ) => {
+    const data = editor.getData();
+    socket.emit('editing', {
+      data: data,
+    });
+  }
+  throttle = (cb, delay) => {
+    let previousCall = new Date().getTime();
+    return function() {
+      let time = new Date().getTime();
+      if ((time - previousCall) >= delay) {
+        previousCall = time;
+        cb.apply(null, arguments);
+      }
+    };
+  }
   render() {
     return (
       <div className="editor">
-        <header className="editor-header">
-          <p className="header-click">
-            Editor page.
-          </p>
-        </header>
+        <CKEditor
+          data={this.state.data}
+          editor={ClassicEditor}
+          onChange={this.throttle(this.onChange, 500)}/>
       </div>
     );
   }
